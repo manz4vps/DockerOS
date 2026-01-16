a5691187e129e82127df4206fcfb59718a6b626e
manz4vps <rismangaming133@gmail.com>
2026-01-16 12:01:43 +0700

    Update install-panel.sh

--- Scrip/install-panel.sh
+++ Scrip/install-panel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 clear
-read -p "Enter domain (e.g., panel.example.com): " DOMAIN
+read -p "🌐 Enter domain (panel.example.com): " DOMAIN
 
 # --- Dependencies ---
 apt update && apt install -y curl apt-transport-https ca-certificates gnupg unzip git tar sudo lsb-release
