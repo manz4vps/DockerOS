5f7035d2a60fd62b11ed00dc2b66e25b1903bb0b
manz4vps <rismangaming133@gmail.com>
2026-02-07 14:33:46 +0700

    Update vm-idx.sh

--- Scrip/vm-idx.sh
+++ Scrip/vm-idx.sh
@@ -20,8 +20,8 @@ NC='\033[0m' # No Color (Reset)
 display_header() {
     clear
     echo -e "${BLUE}  ╔══════════════════════════════════════╗${NC}"
-    echo -e "${BLUE}  ║${CYAN}      VIRTUAL MACHINE MANAGER         ${BLUE}║${NC}"
-    echo -e "${BLUE}  ║${YELLOW}          POWERED BY MANZ             ${BLUE}║${NC}"
+    echo -e "${BLUE}  ║${CYAN}      VIRTUAL MACHINE MANAGER          ${BLUE}║${NC}"
+    echo -e "${BLUE}  ║${YELLOW}          POWERED BY MANZ              ${BLUE}║${NC}"
     echo -e "${BLUE}  ╚══════════════════════════════════════╝${NC}"
     echo
 }
@@ -1086,13 +1086,14 @@ check_dependencies
 VM_DIR="${VM_DIR:-$HOME/vms}"
 mkdir -p "$VM_DIR"
 
-# Supported OS list - UPDATED WITH DEBIAN 13
+# Supported OS list - UPDATED WITH UBUNTU 25.10
 declare -A OS_OPTIONS=(
-    ["Ubuntu 22.04"]="ubuntu|jammy|https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img|ubuntu22|ubuntu|ubuntu"
+    ["Ubuntu 25.10"]="ubuntu|questing|https://cloud-images.ubuntu.com/releases/25.10/release/ubuntu-25.10-server-cloudimg-amd64.img|ubuntu2510|ubuntu|ubuntu"
     ["Ubuntu 24.04"]="ubuntu|noble|https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.img|ubuntu24|ubuntu|ubuntu"
-    ["Debian 11"]="debian|bullseye|https://cloud.debian.org/images/cloud/bullseye/latest/debian-11-generic-amd64.qcow2|debian11|debian|debian"
-    ["Debian 12"]="debian|bookworm|https://cloud.debian.org/images/cloud/bookworm/latest/debian-12-generic-amd64.qcow2|debian12|debian|debian"
+    ["Ubuntu 22.04"]="ubuntu|jammy|https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img|ubuntu22|ubuntu|ubuntu"
     ["Debian 13"]="debian|trixie|https://cloud.debian.org/images/cloud/trixie/daily/latest/debian-13-generic-amd64-daily.qcow2|debian13|debian|debian"
+    ["Debian 12"]="debian|bookworm|https://cloud.debian.org/images/cloud/bookworm/latest/debian-12-generic-amd64.qcow2|debian12|debian|debian"
+    ["Debian 11"]="debian|bullseye|https://cloud.debian.org/images/cloud/bullseye/latest/debian-11-generic-amd64.qcow2|debian11|debian|debian"
     ["Fedora 40"]="fedora|40|https://download.fedoraproject.org/pub/fedora/linux/releases/40/Cloud/x86_64/images/Fedora-Cloud-Base-40-1.14.x86_64.qcow2|fedora40|fedora|fedora"
     ["CentOS Stream 9"]="centos|stream9|https://cloud.centos.org/centos/9-stream/x86_64/images/CentOS-Stream-GenericCloud-9-latest.x86_64.qcow2|centos9|centos|centos"
     ["AlmaLinux 9"]="almalinux|9|https://repo.almalinux.org/almalinux/9/cloud/x86_64/images/AlmaLinux-9-GenericCloud-latest.x86_64.qcow2|almalinux9|alma|alma"
