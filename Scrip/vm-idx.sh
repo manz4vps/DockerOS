96134a51f9da9bd65430db580fe14f1b6ae6b142
manz4vps <rismangaming133@gmail.com>
2026-05-01 00:37:06 +0700

    Correct Debian 12 size in vm-idx.sh
    Updated the size of Debian 12 from 323MB to 333MB.

--- Scrip/vm-idx.sh
+++ Scrip/vm-idx.sh
@@ -868,7 +868,7 @@ OS_ORDER=(
     "Ubuntu 22.04  \033[1;33m|294MB\033[0m"
     "Ubuntu 20.04  \033[1;33m|252MB\033[0m"
     "Debian 13  \033[1;33m|322MB\033[0m"
-    "Debian 12  \033[1;33m|323MB\033[0m"
+    "Debian 12  \033[1;33m|333MB\033[0m"
     "Debian 11  \033[1;33m|252MB\033[0m"
     "CentOS Stream 9  \033[1;33m|1.4GB\033[0m"
     "Fedora 43  \033[1;33m|556MB\033[0m"
